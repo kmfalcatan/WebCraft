faa79a2c67bce27970c4951c0e4f224aa69e1cd1
FirstName Falcatan <kmfalcatan2@gmail.com>
2024-02-01 00:09:15 +0800

    asdjahskdas

--- WebCraft/assets/js/loading.js
+++ WebCraft/assets/js/loading.js
@@ -1,6 +1,6 @@
  // Set the time to wait before transitioning to the next page (in milliseconds)
  const transitionTime = 4000; // 4 seconds
- const redirectToLink = '/WebCraft/index.html'; // Replace this with your local link
+ const redirectToLink = '/WebCraft/admin panel/dashboard.html'; // Replace this with your local link
  
  // Wait for the specified time, then redirect to the local link
  setTimeout(() => {
