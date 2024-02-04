eefa8a8b8b71dd0bcea8310f63f056ac586a2563
powiepow <pawtingkasan20@gmail.com>
2024-02-05 02:13:05 +0800

    compilation

---
+++ WebCraft/admin panel/setting.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../assets/css/setting.css">
+</head>
+<body>
+    <div class="container">
+        <div class="subContainer">
+            <div class="buttonContainer">
+                <a href="../admin panel/editProfile.php">
+                    <button>Edit profile</button>
+                </a>
+            </div>
+
+            <div class="buttonContainer">
+                <a href="../admin panel/changePassword.php">
+                    <button>Change password</button>
+                </a>
+            </div>
+
+            <div class="buttonContainer">
+                <a href="../admin panel/recycle.php">
+                    <button>Recycle</button>
+                </a>
+            </div>
+
+            <div class="buttonContainer">
+                <a href="../authentication/login.php">
+                    <button>Log out</button>
+                </a>
+            </div>
+
+            <div class="buttonContainer">
+                <a href="../admin panel/dashboard.php">
+                    <button>Back</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
