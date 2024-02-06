39551bd94a715597217118ed47a9e57bc31b31a4
FirstName Falcatan <kmfalcatan2@gmail.com>
2024-02-06 10:57:09 +0800

    akjsdbkaksbd

---
+++ WebCraft/user panel/changePassword.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="../assets/css/setting.css">
+    <link rel="stylesheet" href="../assets/css/changePassword.css">
+</head>
+<body>
+    <div class="container">
+        <div class="subContainer1">
+            <div class="inputPassContainer">
+                <input type="text" placeholder="Old password:" required>
+            </div>
+
+            <div class="inputPassContainer">
+                <input type="text" placeholder="New password:" required>
+            </div>
+
+            <div class="inputPassContainer">
+                <input type="text" placeholder="Confirm password:" required>
+            </div>
+
+            <div class="inputPassContainer">
+                <a href="">
+                    <button class="button">Save</button>
+                </a>
+
+                <a href="../admin panel/setting.php">
+                    <button class="button">Back</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
