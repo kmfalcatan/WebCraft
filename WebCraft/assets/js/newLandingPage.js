c9132f80844489582393534f455c2273c1144535
FirstName Falcatan <kmfalcatan2@gmail.com>
2024-02-14 01:24:40 +0800

    askjdbajksd

---
+++ WebCraft/assets/js/newLandingPage.js
@@ -0,0 +1,5 @@
+
+function toggleColorContainer() {
+    var colorContainer = document.querySelector('.colorContainer');
+    colorContainer.style.display = (colorContainer.style.display === 'none' || colorContainer.style.display === '') ? 'block' : 'none';
+}
